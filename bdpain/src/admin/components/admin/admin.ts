9606a10162a81e3b0bce4badee596cdd6de67eec
zaidediak <107957802+zaidediak@users.noreply.github.com>
2023-07-14 20:19:18 -0400

    idrissa

--- bdpain/src/admin/components/admin/admin.ts
+++ bdpain/src/admin/components/admin/admin.ts
@@ -5,21 +5,14 @@ import {
   AdminState,
 } from "./admin_interface";
 import { template } from "./admin_template";
+
 export class Admin
   extends Component<AdminProps, AdminState>
   implements AdminController
 {
   render = template.bind(this, this.props, this.state);
+
   constructor(props: AdminProps) {
     super(props);
   }
 }
-
-
-
-
-
-
-
-
-
