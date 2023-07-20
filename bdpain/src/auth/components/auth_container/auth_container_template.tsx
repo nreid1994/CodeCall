98ffdc13d7ef2d0af2143af0e1013310be4a9554
AllanQZhang <AllanQZhang@gmail.com>
2023-07-19 23:39:24 -0400

    auth_container_template.tsx, auth_container.scss

--- bdpain/src/auth/components/auth_container/auth_container_template.tsx
+++ bdpain/src/auth/components/auth_container/auth_container_template.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import "./auth_container.scss";
 import {
   AuthContainerController,
   AuthContainerProps,
   AuthContainerState,
 } from "./auth_container_interface";
+import Button from 'react-bootstrap/Button';
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Row from "react-bootstrap/Row";
 
 export function template(
   this: AuthContainerController,
@@ -16,7 +21,36 @@ export function template(
   return (
     <Container>
       <Row>
-        <Col className="col-sm-9 col-md-7 col-lg-5 mx-auto">Hi</Col>
+        <Col className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <Card className="card border-0 shadow rounded-3 my-5">
+            <Card.Body className="p-4 p-sm-5">
+              <Card.Title as="h5" className="text-center mb-5 fw-light fs-5">Sign In</Card.Title>
+              <Form className="form-floating mb-3">
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Email address"
+                  className="mb-3"
+                >
+                  <Form.Control type="email"/>
+                </FloatingLabel>
+              </Form>
+              <Form className="form-floating mb-3">
+                <FloatingLabel controlId="floatingPassword" label="Password">
+                  <Form.Control type="password"/>
+                </FloatingLabel>
+              </Form>
+              <Form className="form-check mb-3">
+                <Form.Group>
+                  <Form.Check type="checkbox" value="" id="rememberPasswordCheck"/>
+                  <Form.Label>Remember password</Form.Label>
+                </Form.Group>
+              </Form>
+              <Form className="d-grid">
+                <Button type="submit" className="btn btn-primary btn-login text-uppercase fw-bold">Sign In</Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </Container>
   );
