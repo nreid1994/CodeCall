a093ddc7464756e5db77c371f7a28a210a30b4c6
Kevin Zhang <kevinzhang3082@gmail.com>
2023-07-20 22:01:40 -0400

    Added Sign Up form

--- bdpain/src/contrib/components/user_form/user_form_template.tsx
+++ bdpain/src/contrib/components/user_form/user_form_template.tsx
@@ -6,62 +6,74 @@ import {
 } from "./user_form_interface";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-
+import ProgressBar from 'react-bootstrap/ProgressBar';
 export function template(
   this: UserFormController,
   props: UserFormProps,
   state: UserFormState
 ) {
   return (
-  <>
-    <Card.Title as="h5" className="text-center mb-5 fw-light fs-5">
-      Sign Up
-    </Card.Title>
-    <Form className="form-floating mb-3">
-      <Form.Floating className="mb-3">
-        <Form.Control
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Username"
-          pattern="[a-zA-Z0-9_\-]*"
-          title="Hey your username can only be alphanumeric with dashes and underscores."
-          required
-        />
-        <Form.Label htmlFor="username">Username</Form.Label>
-      </Form.Floating>
-      <Form.Floating className="mb-3">
-        <Form.Control
-          type="email"
-          id="email"
-          name="email"
-          placeholder="name@example.com"
-          required
-        />
-        <Form.Label htmlFor="">Email address</Form.Label>
-      </Form.Floating>
-      <Form.Floating className="mb-3">
-        <Form.Control
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-          required
-        />
-        <Form.Label htmlFor="password">Password</Form.Label>
-      </Form.Floating>
-      <div className="d-grid">
-        <Button
-          type="submit"
-          variant="primary"
-          className="btn-login text-uppercase fw-bold"
-        >
-          Sign Up
-        </Button>
-      </div>
-    </Form>
+    <>
+      <Card.Title as="h5" className="text-center mb-5 fw-light fs-5">
+        Sign Up
+      </Card.Title>
+      <Form className="form-floating mb-3">
+        <Form.Floating className="mb-3">
+          <Form.Control
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+          <Form.Label htmlFor="email">Email</Form.Label>
+        </Form.Floating>
+        <Form.Floating className="mb-3">
+          <Form.Control
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Username"
+            pattern="[a-zA-Z0-9_\-]*"
+            title="Hey, your username can only be alphanumeric with dashes and underscores."
+            required
+          />
+          <Form.Label htmlFor="username">Username</Form.Label>
+        </Form.Floating>
+        <Form.Floating className="mb-3">
+          <Form.Control
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+            title="Hey, your password must be more than 10 characters."
+            required
+          />
+          <Form.Label htmlFor="password">Password</Form.Label>
+        </Form.Floating>
+        <ProgressBar now={50} label={`${50}%`} className="mb-3" />
+        <Form.Floating className="mb-3">
+          <Form.Control
+            type="text"
+            id="captcha"
+            name="captcha"
+            placeholder="captcha"
+            required
+          />
+          <Form.Label htmlFor="email">Captcha: What is 2+2</Form.Label>
+        </Form.Floating>
+        <div className="d-grid">
+          <Button
+            type="submit"
+            variant="primary"
+            className="btn-register text-uppercase fw-bold"
+          >
+            Sign Up
+          </Button>
+        </div>
+      </Form>
     </>
   );
 }
