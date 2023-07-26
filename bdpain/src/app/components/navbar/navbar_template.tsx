fb64726781f4cb461fea657d524ae3f1d6f55816
Kevin Zhang <kevinzhang3082@gmail.com>
2023-07-26 15:51:25 -0400

    navbar added

--- bdpain/src/app/components/navbar/navbar_template.tsx
+++ bdpain/src/app/components/navbar/navbar_template.tsx
@@ -5,10 +5,43 @@ import {
   NavbarState,
 } from "./navbar_interface";
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 export function template(
   this: NavbarController,
   props: NavbarProps,
   state: NavbarState
 ) {
-  return <div>Im The Navbar Page</div>;
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            </Form>
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action1">Opportunity</Nav.Link>
+          </Nav>
+          <Button>Login</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- bdpain/src/auth/components/auth_container/auth_container_template.tsx
+++ bdpain/src/auth/components/auth_container/auth_container_template.tsx
@@ -14,6 +14,7 @@ import { UserForm } from "../../../contrib/components/user_form/user_form";
 import { AuthFindAccount } from "../auth_recovery/auth_find_account/auth_find_account";
 import { AuthChangePassword } from "../auth_recovery/auth_change_password/auth_change_password";
 import { OpportunityCard } from "../../../opportunity/components/opportunity_card/opportunity_card";
+import { Navbar } from "../../../app/components/navbar/navbar"
 
 export function template(
   this: AuthContainerController,
@@ -21,17 +22,20 @@ export function template(
   state: AuthContainerState
 ) {
   return (
-    <Container>
-      <Row>
-        <Col className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-          <Card className="card border-0 shadow rounded-3 my-5">
-            <Card.Body className="p-4 p-sm-5">
-              <OpportunityCard />
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Navbar/>
+      <Container>
+        <Row>
+          <Col className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <Card className="card border-0 shadow rounded-3 my-5">
+              <Card.Body className="p-4 p-sm-5">
+                <OpportunityCard />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
