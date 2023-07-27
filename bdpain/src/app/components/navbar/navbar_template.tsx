ef15f88dbf5e8a6408072dc85beab94a9570b883
AllanQZhang <AllanQZhang@gmail.com>
2023-07-27 17:40:11 -0400

    fixing Navbar

--- bdpain/src/app/components/navbar/navbar_template.tsx
+++ bdpain/src/app/components/navbar/navbar_template.tsx
@@ -25,33 +25,33 @@ export function template(
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Col></Col>
-            <Image src={BDPA} className='' style={{width : '1.5%'}}></Image>
-            <Navbar.Brand href="#">BDPAIn</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-              >
-                <Form className="d-flex">
+        <Col className="col-3"></Col>
+          <Image src={BDPA} className='me-2' style={{height: 30}}></Image>
+          <Navbar.Brand href="#" className="me-3">BDPAIn</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Form className="d-flex">
                 <Form.Control
                   type="search"
                   placeholder="Search"
                   className="pe-5"
                   aria-label="Search"
                 />
-                </Form>
-              </Nav>
-              <Nav.Link href="#" className='me-2 p-2'>Home</Nav.Link>
-              <Nav.Link href="#" className='me-2 p-2'>Opportunities</Nav.Link>
-              <Nav.Link href="#" className='me-2 p-2'>Admin</Nav.Link>
-              <Nav.Link href="#" className='me-2 p-2'>Profile</Nav.Link>
-              <div className="vr me-3" />
-              <Button>Login</Button>
-            </Navbar.Collapse>
-          <Col></Col>
+              </Form>
+            </Nav>
+            <Nav.Link href="#" className='me-3'>Home</Nav.Link>
+            <Nav.Link href="#" className='me-3'>Opportunities</Nav.Link>
+            <Nav.Link href="#" className='me-3'>Admin</Nav.Link>
+            <Nav.Link href="#" className='me-3'>Profile</Nav.Link>
+            <div className="vr me-3" />
+            <Button>Login</Button>
+          </Navbar.Collapse>
+        <Col className="col-3"></Col>
       </Container>
     </Navbar>
   );
