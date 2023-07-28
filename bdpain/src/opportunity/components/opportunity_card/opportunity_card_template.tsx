b4d642a9362613c66eb93d94a52b16f70aefca71
AllanQZhang <AllanQZhang@gmail.com>
2023-07-28 17:10:36 -0400

    Opportunity cards, views and active viewers

--- bdpain/src/opportunity/components/opportunity_card/opportunity_card_template.tsx
+++ bdpain/src/opportunity/components/opportunity_card/opportunity_card_template.tsx
@@ -14,109 +14,45 @@ export function template(
 ) {
 return (
   <>
-    <div className="border-bottom row">
+    <div className="row">
       <div className="col-1">
         <Image src="../../../favicon.ico" className="mt-4" style={{height: 40}}></Image>
       </div>
-      <div className="col-10">
+      <div className="col-10 position-relative p-0">
         <Card.Body>
-        <Card.Title className="fs-3 mx-1 text-primary">The Job</Card.Title>
-        <Card.Subtitle className="mb-1 mx-1 ">Company</Card.Subtitle>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Location
-        </Card.Text>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Pay
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted">
-            Date
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted fs-5">
-            ·
-        </Card.Text>
-        <Card.Text className="d-inline text-success">
-            Applicants
-        </Card.Text>
+          <Card.Title className="fs-3 text-primary">The Job</Card.Title>
+          <Card.Subtitle className="mb-1 ">Company</Card.Subtitle>
+          <span className="d-block">
+            <Card.Text className="mb-1 text-muted">
+              Location
+            </Card.Text>
+            <Card.Text className="mb-1 text-muted">
+              Pay
+            </Card.Text>
+            <Card.Text className="d-inline me-1 text-muted">
+              Date
+            </Card.Text>
+            <Card.Text className="d-inline me-1 text-muted fs-5">
+              ·
+            </Card.Text>
+            <Card.Text className="d-inline me-1 text-success">
+              Applicants
+            </Card.Text>
+            <span className="d-inline position-absolute end-0 me-3">
+              <Card.Text className="d-inline me-1 text-muted">
+                Views
+              </Card.Text>
+              <Card.Text className="d-inline me-1 text-muted fs-5">
+                ·
+              </Card.Text>
+              <Card.Text className="d-inline text-muted">
+                Active Viewers
+              </Card.Text>
+            </span>
+          </span>
         </Card.Body>
       </div>
-    </div>
-    <div className="border-bottom row">
-      <div className="col-1">
-        <Image src="../../../favicon.ico" className="mt-4" style={{height: 40}} roundedCircle></Image>
-      </div>
-      <div className="col-10">
-        <Card.Body>
-        <Card.Title className="fs-3 text-primary">The Job</Card.Title>
-        <Card.Subtitle className="mb-1 mx-1 ">Company</Card.Subtitle>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Location
-        </Card.Text>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Pay
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted">
-            Date
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted fs-5">
-            ·
-        </Card.Text>
-        <Card.Text className="d-inline text-success">
-            Applicants
-        </Card.Text>
-        </Card.Body>
-      </div>
-    </div>
-    <div className="border-bottom row">
-      <div className="col-1">
-        <Image src="../../../logo192.png" className="mt-4" style={{height: 40}}></Image>
-      </div>
-      <div className="col-10">
-        <Card.Body>
-        <Card.Title className="fs-3 text-primary">The Job</Card.Title>
-        <Card.Subtitle className="mb-1 mx-1 ">Company</Card.Subtitle>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Location
-        </Card.Text>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Pay
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted">
-            Date
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted fs-5">
-            ·
-        </Card.Text>
-        <Card.Text className="d-inline text-success">
-            Applicants
-        </Card.Text>
-        </Card.Body>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-1">
-        <Image src="../../../logo512.png" className="mt-4" style={{height: 40}}></Image>
-      </div>
-      <div className="col-10">
-        <Card.Body>
-        <Card.Title className="fs-3 text-primary">The Job</Card.Title>
-        <Card.Subtitle className="mb-1 mx-1 ">Company</Card.Subtitle>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Location
-        </Card.Text>
-        <Card.Text className="mb-1 mx-1 text-muted">
-            Pay
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted">
-            Date
-        </Card.Text>
-        <Card.Text className="d-inline me-1 text-muted fs-5">
-            ·
-        </Card.Text>
-        <Card.Text className="d-inline text-success">
-            Applicants
-        </Card.Text>
-        </Card.Body>
-      </div>
+      <div className="col-1"></div>
     </div>
   </>
   );
